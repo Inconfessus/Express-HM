e19fd4ab23403d0194a618e5eee65eb10dfa5388
Inconfessus <lalo-cortes@outlook.com>
2021-03-23 20:42:36 -0600

    mysql database implementation

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
+const pokemon = require('./routes/pokemon')
 
-const { pokemon } = require('./pokedex.json');
-
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
 
 app.get("/", (req, res, next) =>{
     return res.status(200).send("Bienvenido al Pokedex");
 })
 
-app.post("/pokemon", (req, res, next) =>{   
-    return res.status(200).send(req.body);
-});  
-
-app.get("/pokemon", (req, res, next) =>{   
-    return res.status(200).send(pokemon);
-});
-
-app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) =>{   
-    const id = req.params.id -1;
-
-  return (id >= 0 && id <= 150 ) ? 
-    res.status(200).send(pokemon[req.params.id -1]) : 
-    res.status(404).send("Ese Pokemon forma parte de otra generación"); 
-
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) =>{   
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p)=>{
-        return (p.name.toUpperCase() == name.toUpperCase()) ? p: null;
-        
-    });
-
-    (pk.length>0) ? 
-        res.status(200).send(pk): 
-        res.status(404).send("Pokemon no encontrado");
-    
-}); 
+app.use("/pokemon",pokemon);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...")
