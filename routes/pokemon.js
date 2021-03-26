f91ef6d7fb0ed7b00df3123004465783dec16e1b
Inconfessus <lalo-cortes@outlook.com>
2021-03-25 19:12:09 -0600

    body parser changes

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,7 +8,7 @@ pokemon.post("/", (req, res, next) =>{
 
 pokemon.get("/", async (req, res, next) =>{   
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({code: 1, message: pkmn});
 });
 
 pokemon.get("/:id([0-9]{1,3})",async (req, res, next) =>{
@@ -17,8 +17,8 @@ pokemon.get("/:id([0-9]{1,3})",async (req, res, next) =>{
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id =" + id);
     
   return (id > 0 && id <= 722 ) ? 
-    res.status(200).json(pkmn) : 
-    res.status(404).send("Ese Pokemon forma parte de otra generación"); 
+    res.status(200).json({ code: 1, message: pkmn}) : 
+    res.status(404).json({code: 404, message: "Pokemon no encontrado"}); 
 
 });
 
@@ -27,8 +27,8 @@ pokemon.get("/:name([A-Za-z '-]+)", async (req, res, next) =>{
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name =" + "'" + name.toLowerCase() + "'");
    
     (pkmn.length) ? 
-        res.status(200).json(pkmn):
-        res.status(404).send("Pokemon no encontrado"); 
+        res.status(200).json({ code: 1, message: pkmn}) :
+        res.status(404).json({code: 404, message: "Pokemon no encontrado"}); 
     
 }); 
 
