8d1310ed8b0dc2de674e2374bf0e15fe7fd8a725
Inconfessus <lalo-cortes@outlook.com>
2021-04-20 21:02:43 -0500

    Added Users

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 const pokemon = require('./routes/pokemon')
+const user = require('./routes/user')
 
 app.use(morgan('dev'));
 app.use(express.json(app.use(express.urlencoded({ extended: true }))));
 
 
-app.get("/", (req, res, next) =>{
-    return res.status(200).json({code: 1, message:"Bienvenido al Pokedex"});
+app.get("/", (req, res, next) => {
+    return res.status(200).json({ code: 1, message: "Bienvenido al Pokedex" });
 })
 
-app.use("/pokemon",pokemon);
+app.use("/pokemon", pokemon);
 
-app.use((req, res , next) => {
-    return res.status(404).json({ code: 404, message: "URL no encontrada"})
+app.use("/user", user);
+
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: "URL no encontrada" })
 
 });
 
